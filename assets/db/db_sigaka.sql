1445709b4418fee89987b02b9d8448dbe4177eae
jbenastey <jbenastey@gmail.com>
2019-07-15 14:56:13 +0700

    finishing

--- assets/db/db_sigaka.sql
+++ assets/db/db_sigaka.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 30 Jun 2019 pada 16.29
+-- Waktu pembuatan: 15 Jul 2019 pada 09.55
 -- Versi server: 10.1.35-MariaDB
 -- Versi PHP: 7.2.9
 
@@ -32,6 +32,7 @@ CREATE TABLE `sigaka_absen` (
   `absen_id` varchar(20) NOT NULL,
   `absen_karyawan_id` varchar(20) NOT NULL,
   `absen_hari` varchar(10) NOT NULL,
+  `absen_status` enum('lembur','normal') NOT NULL DEFAULT 'normal',
   `absen_date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -39,9 +40,9 @@ CREATE TABLE `sigaka_absen` (
 -- Dumping data untuk tabel `sigaka_absen`
 --
 
-INSERT INTO `sigaka_absen` (`absen_id`, `absen_karyawan_id`, `absen_hari`, `absen_date_created`) VALUES
-('ABS-20906', 'PEG-31293', 'Sabtu', '2019-06-29 22:08:26'),
-('ABS-21215', 'PEG-21053', 'Sabtu', '2019-06-29 22:13:35');
+INSERT INTO `sigaka_absen` (`absen_id`, `absen_karyawan_id`, `absen_hari`, `absen_status`, `absen_date_created`) VALUES
+('ABS-74757', 'PEG-74722', 'Senin', 'lembur', '2019-07-15 14:12:37'),
+('ABS-76293', 'PEG-76226', 'Senin', 'normal', '2019-07-15 14:38:13');
 
 -- --------------------------------------------------------
 
@@ -51,8 +52,12 @@ INSERT INTO `sigaka_absen` (`absen_id`, `absen_karyawan_id`, `absen_hari`, `abse
 
 CREATE TABLE `sigaka_gaji` (
   `gaji_id` varchar(20) NOT NULL,
-  `gaji_jabatan` varchar(255) DEFAULT NULL,
-  `gaji_jumlah` varchar(255) DEFAULT NULL,
+  `gaji_karyawan_id` varchar(20) NOT NULL,
+  `gaji_lembur` int(20) DEFAULT '0',
+  `gaji_total` int(20) NOT NULL,
+  `gaji_tanggal` date DEFAULT NULL,
+  `gaji_bulan_ke` int(11) DEFAULT NULL,
+  `gaji_status` enum('sudah','belum') NOT NULL DEFAULT 'belum',
   `gaji_date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -60,9 +65,30 @@ CREATE TABLE `sigaka_gaji` (
 -- Dumping data untuk tabel `sigaka_gaji`
 --
 
-INSERT INTO `sigaka_gaji` (`gaji_id`, `gaji_jabatan`, `gaji_jumlah`, `gaji_date_created`) VALUES
-('GJI-24796', 'Penyanyi', '50000', '2019-06-27 15:39:56'),
-('GJI-24987', 'Pemulung', '90000', '2019-06-27 15:43:07');
+INSERT INTO `sigaka_gaji` (`gaji_id`, `gaji_karyawan_id`, `gaji_lembur`, `gaji_total`, `gaji_tanggal`, `gaji_bulan_ke`, `gaji_status`, `gaji_date_created`) VALUES
+('GJI-74757', 'PEG-74722', 35000, 35000, '2019-07-15', 1, 'sudah', '2019-07-15 14:12:37'),
+('GJI-76293', 'PEG-76226', 0, 35000, '2019-07-15', 1, 'belum', '2019-07-15 14:38:13');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `sigaka_jabatan`
+--
+
+CREATE TABLE `sigaka_jabatan` (
+  `jabatan_id` varchar(20) NOT NULL,
+  `jabatan_nama` varchar(255) DEFAULT NULL,
+  `jabatan_gaji` int(20) DEFAULT NULL,
+  `jabatan_date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `sigaka_jabatan`
+--
+
+INSERT INTO `sigaka_jabatan` (`jabatan_id`, `jabatan_nama`, `jabatan_gaji`, `jabatan_date_created`) VALUES
+('JAB-74569', 'Frontliner', 35000, '2019-07-15 14:09:29'),
+('JAB-76143', 'frontliner', 35000, '2019-07-15 14:35:43');
 
 -- --------------------------------------------------------
 
@@ -72,13 +98,14 @@ INSERT INTO `sigaka_gaji` (`gaji_id`, `gaji_jabatan`, `gaji_jumlah`, `gaji_date_
 
 CREATE TABLE `sigaka_karyawan` (
   `karyawan_id` varchar(20) NOT NULL,
-  `karyawan_gaji_id` varchar(20) DEFAULT NULL,
+  `karyawan_jabatan_id` varchar(20) DEFAULT NULL,
   `karyawan_nama` varchar(255) DEFAULT NULL,
   `karyawan_tempat_lahir` varchar(255) DEFAULT NULL,
   `karyawan_tanggal_lahir` date DEFAULT NULL,
   `karyawan_alamat` text,
   `karyawan_tanggal_gabung` date DEFAULT NULL,
   `karyawan_nomor_hp` varchar(20) DEFAULT NULL,
+  `karyawan_no_rekening` varchar(30) DEFAULT NULL,
   `karyawan_date_created` datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -86,10 +113,9 @@ CREATE TABLE `sigaka_karyawan` (
 -- Dumping data untuk tabel `sigaka_karyawan`
 --
 
-INSERT INTO `sigaka_karyawan` (`karyawan_id`, `karyawan_gaji_id`, `karyawan_nama`, `karyawan_tempat_lahir`, `karyawan_tanggal_lahir`, `karyawan_alamat`, `karyawan_tanggal_gabung`, `karyawan_nomor_hp`, `karyawan_date_created`) VALUES
-('PEG-21053', 'GJI-24796', 'Dian Wulandari', 'Disana', '2019-06-04', 'disana juga', '2019-06-19', '12312313', '2019-06-29 22:10:53'),
-('PEG-31293', 'GJI-24796', 'Jihad Benastey', 'Pekanbaru', '2019-06-03', 'Jalan Yudha Karya', '2019-06-26', '085355825959', '2019-06-27 17:28:13'),
-('PEG-60191', 'GJI-24987', 'Bagus Sujatmiko', 'Tembilahan', '2019-06-04', 'Arengka', '2019-06-27', '1123123123', '2019-06-28 01:29:51');
+INSERT INTO `sigaka_karyawan` (`karyawan_id`, `karyawan_jabatan_id`, `karyawan_nama`, `karyawan_tempat_lahir`, `karyawan_tanggal_lahir`, `karyawan_alamat`, `karyawan_tanggal_gabung`, `karyawan_nomor_hp`, `karyawan_no_rekening`, `karyawan_date_created`) VALUES
+('PEG-74722', 'JAB-74569', 'Cucu', 'Pekanbaru', '1997-09-04', 'Disana', '2014-06-14', '081234567890', '', '2019-07-15 14:12:02'),
+('PEG-76226', 'JAB-76143', 'widi', 'pku', '2000-07-09', 'pku', '2019-07-24', '081233444433', '', '2019-07-15 14:37:06');
 
 -- --------------------------------------------------------
 
@@ -112,7 +138,29 @@ CREATE TABLE `sigaka_pengguna` (
 --
 
 INSERT INTO `sigaka_pengguna` (`pengguna_id`, `pengguna_username`, `pengguna_password`, `pengguna_nama`, `pengguna_foto`, `pengguna_hak_akses`, `pengguna_date_created`) VALUES
-(1, 'manajer', '5fbb69663039903931daf8b26344b76b', 'Mirwansyah', NULL, 'manajer', '2019-06-28 21:06:43');
+(1, 'manajer', '5fbb69663039903931daf8b26344b76b', 'Abdul Mustaqim', NULL, 'manajer', '2019-06-28 21:06:43'),
+(2, 'owner', '5be057accb25758101fa5eadbbd79503', 'Saptoni Khusni', NULL, 'owner', '2019-07-15 12:27:55');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `sigaka_pinjam`
+--
+
+CREATE TABLE `sigaka_pinjam` (
+  `pinjam_id` varchar(20) NOT NULL,
+  `pinjam_karyawan_id` varchar(20) NOT NULL,
+  `pinjam_jumlah` int(20) NOT NULL,
+  `pinjam_bayar` int(20) NOT NULL DEFAULT '0',
+  `pinjam_date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `sigaka_pinjam`
+--
+
+INSERT INTO `sigaka_pinjam` (`pinjam_id`, `pinjam_karyawan_id`, `pinjam_jumlah`, `pinjam_bayar`, `pinjam_date_created`) VALUES
+('PJM-74863', 'PEG-74722', 50000, 50000, '2019-07-15 14:14:23');
 
 --
 -- Indexes for dumped tables
@@ -131,6 +179,12 @@ ALTER TABLE `sigaka_gaji`
   ADD PRIMARY KEY (`gaji_id`);
 
 --
+-- Indeks untuk tabel `sigaka_jabatan`
+--
+ALTER TABLE `sigaka_jabatan`
+  ADD PRIMARY KEY (`jabatan_id`);
+
+--
 -- Indeks untuk tabel `sigaka_karyawan`
 --
 ALTER TABLE `sigaka_karyawan`
@@ -143,6 +197,12 @@ ALTER TABLE `sigaka_pengguna`
   ADD PRIMARY KEY (`pengguna_id`);
 
 --
+-- Indeks untuk tabel `sigaka_pinjam`
+--
+ALTER TABLE `sigaka_pinjam`
+  ADD PRIMARY KEY (`pinjam_id`);
+
+--
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
@@ -150,7 +210,7 @@ ALTER TABLE `sigaka_pengguna`
 -- AUTO_INCREMENT untuk tabel `sigaka_pengguna`
 --
 ALTER TABLE `sigaka_pengguna`
-  MODIFY `pengguna_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `pengguna_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
